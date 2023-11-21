9eac438d0d05431aab85d0e4f8db29f541b218c1
RamonWebDev <ramonplaysgames@gmail.com>
2023-11-20 22:55:24 -0600

    added variables

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ document.addEventListener('DOMContentLoaded', function(){
         yearElement.textContent = currentYear;
     }
 })
+
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
