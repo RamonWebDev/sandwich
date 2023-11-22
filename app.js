e1847dcb3a0705afe0951ca9d66b44c2f3221225
RamonWebDev <ramonplaysgames@gmail.com>
2023-11-21 22:44:10 -0600

    added code to open and close cart

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ document.addEventListener('DOMContentLoaded', function(){
 let cartIcon = document.querySelector("#cart-icon");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
+
+//open cart 
+cartIcon.onclick = function(){
+    cart.classList.add("active");
+}
+
+//close cart 
+closeCart.onclick = function(){
+    cart.classList.remove("active");
+}
