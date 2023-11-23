ffe52e34d8ff6006d37699e4b36e85114bc5ed2e
RamonWebDev <ramonplaysgames@gmail.com>
2023-11-22 23:16:43 -0600

    working on cart code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Function to get year and place in copyright
 document.addEventListener('DOMContentLoaded', function(){
     let yearElement = document.getElementById('copyright-year');
     if(yearElement){
@@ -18,4 +19,63 @@ cartIcon.onclick = function(){
 //close cart 
 closeCart.onclick = function(){
     cart.classList.remove("active");
+}
+
+//Cart Working JS 
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready();
+}
+
+function ready(){
+    //Remove items from Cart
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons)
+    for(let i = 0; i < removeCartButtons.length; i++){
+        var button = removeCartButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity');
+    for(let i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+}
+
+//remove items from cart
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove()
+    updateTotal()
+}
+
+//quantityChanged
+function quantityChanged(event){
+    var input = event.target
+    if(isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateTotal()
+}
+
+//update Total
+function updateTotal(){
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var total = 0;
+    for(let i = 0; i < cartBoxes.length; i++){
+        var cartBox = cartBoxes[i]
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0]
+        var price = parseFloat(priceElement.innerText.replace("$", ""));
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
+
+        //If price contains some cent value
+        total = Math.round(total * 100) /100
+
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total
+    }
 }
